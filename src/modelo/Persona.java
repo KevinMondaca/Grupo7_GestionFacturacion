38cd0479bcee7e640874954948bed6b10e952095
localuser <localuser@CS0R1>
2022-11-04 12:24:24 -0300

    Creacion Superclase persona

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -1,5 +1,21 @@
 package modelo;
-//tes
-public class Persona {
-    
+
+public abstract class Persona {
+
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private int dni;
+    private int telefono;
+
+    public Persona(String n, String a, int d, int t, String e) {
+        this.nombres = n;
+        this.apellidos = a;
+        this.email = e;
+        this.dni = d;
+        this.telefono = t;
+    }
+
+    public abstract void MostrarPersona();
+
 }
